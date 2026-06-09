c5dccc48d321a5e99660aebe0fd31e8a7ad9841a
fp024 <aopenasus@naver.com>
2026-06-09 23:00:44 +0900

    JDT 직렬화 버전 경고 옵션 설정 테스트
    vscode-java-setup-scripts에 기능을 추가하게되서..
    잘 동작하는지 확인해봤다..
    
    잘된다..😊

--- src/main/java/org/fp024/security/domain/CustomUser.java
+++ src/main/java/org/fp024/security/domain/CustomUser.java
@@ -6,12 +6,13 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 public class CustomUser extends User {
+
   /**
    * Spring Security 클래스에 대한 전역 직렬화 값입니다. <br>
    * 주의. 클래스는 다른 버전 간에 직렬화할 수 없습니다. <br>
    * 여전히 직렬 버전이 필요한 이유는 SEC-1709를 참조하십시오.
    */
-  @Serial private static final long serialVersionUID = -7527477803279659303L;
+  @Serial private static final long serialVersionUID = -5686993185897337641L;
 
   public CustomUser(MemberDTO dto) {
     super(
